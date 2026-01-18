60bde4013286363318b20d9b4ef68f1f73558908
Gabriel Miranda Rodrigues (AVANADE INC) <v-gabrielmi@microsoft.com>
2026-01-18 14:23:50 -0300

    Add ORM mapping configurations and default context

---
+++ template/backend/src/Ambev.DeveloperEvaluation.ORM/Mapping/SaleItemConfiguration.cs
@@ -0,0 +1,49 @@
+using Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ambev.DeveloperEvaluation.ORM.Mapping;
+
+public class SaleItemConfiguration : IEntityTypeConfiguration<SaleItem>
+{
+    public void Configure(EntityTypeBuilder<SaleItem> builder)
+    {
+        builder.ToTable("SaleItems");
+
+        builder.HasKey(i => i.Id);
+        builder.Property(i => i.Id)
+            .HasColumnType("uuid")
+            .HasDefaultValueSql("gen_random_uuid()");
+
+        builder.Property(i => i.SaleId)
+            .IsRequired();
+
+        builder.Property(i => i.Product)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(i => i.Quantity)
+            .IsRequired();
+
+        builder.Property(i => i.UnitPrice)
+            .HasColumnType("decimal(18,2)")
+            .IsRequired();
+
+        builder.Property(i => i.Discount)
+            .HasColumnType("decimal(5,2)")
+            .IsRequired()
+            .HasDefaultValue(0);
+
+        builder.Property(i => i.TotalAmount)
+            .HasColumnType("decimal(18,2)")
+            .IsRequired();
+
+        builder.Property(i => i.IsCancelled)
+            .IsRequired()
+            .HasDefaultValue(false);
+
+        builder.HasOne(i => i.Sale)
+            .WithMany(s => s.Items)
+            .HasForeignKey(i => i.SaleId);
+    }
+}
