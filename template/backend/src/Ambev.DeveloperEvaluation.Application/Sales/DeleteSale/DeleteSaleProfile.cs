b3f0b8d8e96f43ce1f7b4df8eff33ce82d4c9fa1
Gabriel Miranda Rodrigues (AVANADE INC) <v-gabrielmi@microsoft.com>
2026-01-18 18:32:37 -0300

    Add DeleteSale Command, Handler, Profile, Result and Validator

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/DeleteSale/DeleteSaleProfile.cs
@@ -0,0 +1,12 @@
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.DeleteSale;
+
+public class DeleteSaleProfile : Profile
+{
+    public DeleteSaleProfile()
+    {
+        CreateMap<Guid, DeleteSaleCommand>()
+            .ConstructUsing(id => new DeleteSaleCommand(id));
+    }
+}
